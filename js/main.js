daed6eb6a16f52933dd616c6d53f589019bb3a02
Grigor <grigdevelop@gmail.com>
2017-08-30 16:29:48 +0200

    ruby page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var app = angular.module("cvApp", ["ngRoute"]);
-app.config(function($routeProvider){
+app.config(function($routeProvider, $locationProvider){
     $routeProvider
         .when("/", {
             templateUrl: "pages/home/home.html"
+        })
+        .when("/ruby", {
+            templateUrl: "pages/ruby/ruby.html"
         });
+
+        // use the HTML5 History API
+        //$locationProvider.html5Mode(true);
 });
